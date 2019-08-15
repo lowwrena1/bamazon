f861998f8b511a7eb8f275c831948fea9f6e477c
Lorena Ontiveros <lorena0328o@gmail.com>
2019-08-14 20:59:46 -0500

    created mysql and inquirer require request, and posted info for database info

---
+++ javascript/bamazon.js
@@ -0,0 +1,10 @@
+var mysql = require ("mysql");
+var inquirer = require ("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: root,
+    password: Elpaso30,
+    database: tbd,
+});
