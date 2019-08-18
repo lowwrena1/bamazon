d63612bd947c02e092ab5e1c649c84b6836416e6
Lorena Ontiveros <lorena0328o@gmail.com>
2019-08-18 18:55:17 -0500

    Everything worksgit add -Agit add -Agit add -A!

--- bamazon.js
+++ bamazon.js
@@ -11,16 +11,138 @@ var connection = mysql.createConnection({
 //connect to mysql sever and database
 // connect to the mysql server and sql database
 connection.connect(function(err) {
-    if (err) throw err;
+	if (err) throw err;
+	console.log("connected as id " + connection.threadId);
+    displayItems();
     // run the start function after the connection is made to prompt the user
-    start();
+   // start();
   });
-var start = function(){
-inquirer.prompt({
-	name:"idAndHowMany",
-	type:"rawlist",
-	message: "Welcom to bamazon!! would you please select the item id of the object you like to pruchase.",
-	choices:["Ttem ID", "How Many?"]
-})
+//var start = function(){
+	// Display Inventory from database once MySQL connection is established
+function displayItems() {
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        // TIP to have all products in a nice TABLE 
+            // => console.table(res);
+                // => This shows all items in a table on Terminal. It looks clean, but index numbers also appear. This is just another method to list the products on Terminal.
+
+        // Welcome Message and Show Products List with information
+        console.log(" WELCOME TO BAMAZON \n");
+        
+
+        for (var i = 0; i < res.length; i++) {
+            console.log (
+                "Item ID: " + res[i].item_id + " | Product Name: " + res[i].product_name + " | Department Name: " + res[i].department_name + " | Price: $" + res[i].price + " | Stock Quantity: " + res[i].stock_quantity +
+                "\n------------------------------------------------------------------------------------------------------------------------------------"
+            );   
+            console.log("------------------------------------------------------------------------------------------------------------------------------------");
+        };
+
+        purchasePrompt();
+    });
 };
- 
+
+// Prompt user to enter ITEM ID the user wants to purchase, and QUANTITY of chosen Item ID 
+function purchasePrompt () {
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+  
+        inquirer.prompt([
+            {
+                name: "id",
+                type: "input",
+                message: "Please enter Item ID you would like to purchase => ",
+
+                // Validate input for whole non-zero number
+                validate: function(value) {
+                    if (isNaN(value) === false) {
+                      return true;
+                    }
+                    return "Please enter a valid item ID with whole non-zero number.";
+                }
+            },
+            {
+                name: "quantity",
+                type: "input",
+                message: "Please enter quantity of selected item you would like to purchase => ",
+
+                // Validate input for whole non-zero number
+                validate: function(value) {
+                    if (isNaN(value) === false) {
+                      return true;
+                    }
+                    return "Please enter a valid quantity of chosen item with whole non-zero number.";
+                }
+            }
+        ]).then(function(answer){
+
+            // Declare Variables to calculate stock level
+                // 1st product's index number is 0. Make sure to minus one from the ID
+            var wantToBuy = res[answer.id-1];
+            var currentStock = res[answer.id-1].stock_quantity;
+            var wantedQuantity = answer.quantity;
+            var remainQuantity = currentStock - wantedQuantity;
+
+            // If the quantity desired by the user is in the stock,
+            if (currentStock >= wantedQuantity) {
+                connection.query (
+                    // this updates the quantity 
+                    "UPDATE products SET ? WHERE ?",
+                    [
+                        {
+                            stock_quantity: remainQuantity,
+                        },
+                        {
+                            item_id: wantToBuy.item_id,
+                        }
+                    ], 
+                    function (err, res) {
+                        if (err) throw err;
+                            console.log (
+                               
+                                "\n*** Your Order Summary ***" + 
+                                "\n------------------------------------------------------------------------------------------------------------------------------------" + 
+                                "\nItem ID: " + wantToBuy.item_id + 
+                                "\nProduct Name: " + wantToBuy.product_name + 
+                                "\nDepartment: " + wantToBuy.department_name + 
+                                "\nUnit Price: $" + wantToBuy.price + 
+                                "\nPurchased Quantity: " + wantedQuantity + 
+                                "\n------------------------------------------------------------------------------------------------------------------------------------" +
+                                "\n<< Updated Stock Quantity for " + wantToBuy.product_name + ": " + remainQuantity + " left >>" +
+                                "\n------------------------------------------------------------------------------------------------------------------------------------"
+                            );
+
+                            var totalCost = (wantToBuy.price * wantedQuantity).toFixed(2);
+
+                            console.log (
+                                "\n------------------------------------------------------------------------------------------------------------------------------------" +
+                                "\n*** Your Total is $" + totalCost + " ***" +
+                                "\n------------------------------------------------------------------------------------------------------------------------------------" +
+                                "\n*** Your Order has been placed! ***" +
+                                "\n*** Thank you for shopping with us! ***" +
+                                "\n------------------------------------------------------------------------------------------------------------------------------------"
+                            );
+                    }
+                ); 
+
+            // If the quantity desired by the user is NOT in the stock,
+            } else {
+                console.log(
+                    "\n------------------------------------------------------------------------------------------------------------------------------------" +
+                    "\n*** We are very Sorry! We currently do not have enough stock at this moment ***" +
+                    "\n------------------------------------------------------------------------------------------------------------------------------------"
+                );
+
+                console.log(
+                    "\n------------------------------------------------------------------------------------------------------------------------------------" +
+                    "\n*** Please Modify your Order. ***" +
+                    "\n------------------------------------------------------------------------------------------------------------------------------------"
+                );
+            }
+            
+            // End the Database Connection
+            connection.end();
+
+        });
+    });
+}
--- mysql.sql
+++ mysql.sql
@@ -3,7 +3,27 @@ create table products(
 item_id int not null auto_increment,
 product_name varchar(100) not null,
 department_name varchar(50) not null,
-price int,
+price float (6,2),
 stock_quantity int,
 primary key (item_id)
 );
+insert into products(product_name, department_name, price, stock_quantity)
+values ("couch", "furniture", 860, 7);
+insert into products(product_name, department_name, price, stock_quantity)
+values ("God of war", "video games", 60, 45);
+insert into products(product_name, department_name, price, stock_quantity)
+values ("deadpool", "movies", 25, 30);
+insert into products(product_name, department_name, price, stock_quantity)
+values ("lamp", "furniture", 20, 15);
+insert into products(product_name, department_name, price, stock_quantity)
+values("javascript for dummies", "literature", 30, 28);
+insert into products(product_name, department_name, price, stock_quantity)
+values("the handmaids tale", "literature", 20, 60);
+insert into products(product_name, department_name, price, stock_quantity)
+values("Mario Party", "video games", 60, 45);
+insert into products(product_name, department_name, price, stock_quantity)
+values("ps4", "electronics", 200, 100);
+insert into products(product_name, department_name, price, stock_quantity)
+values("End Game Blu-ray/DVD", "electronics",25 ,200 );
+insert into products(product_name, department_name, price, stock_quantity)
+values("Spiderman Far From Home", "electronics",25 ,205 );
